03c083fedd160e3793588dbb33f7f73c12d481dd
SmilingJun <smiling.jun@icloud.com>
2017-07-10 22:06:17 -0400

    kit1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+
+  end
+
+  def edit
+    @user.create_profile unless @user.profile
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Successfully Update"
+      redirect_to edit_user_path
+    else
+      render "edit"
+    end
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty])
+  end
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,17 @@ Rails.application.routes.draw do
   namespace :admin do
     root "events#index"
     resources :events
+    resources :users do
+      resource :profile, :controller => "user_profiles"
+    end
   end
 
   root "events#index"
+
+  get "/faq" => "pages#faq"
+
+  resource :user
+
+
 
 end
